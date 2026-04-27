sensille/bpftrace

commit 4a3b4dec7cbb903c307c10ee4de9036c36576554

cleanup: aggregate function info into mockable interfaces
Instead of relying on various interfaces and a mockable "ProbeMatcher"
hanging off the `BPFtrace` object, use a more well-defined
`KernelFunctionInfo` and `UserFunctionInfo` API. These are exposed to
passes via a `FunctionInfo` object that should be placed manually into
the pass context. This is now done in main, and mocks are manually
placed in all test files.

Signed-off-by: Adin Scannell <amscanne@meta.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4968, branch: user/amscanne/funcidx/5

src/probe_matcher.cpp
@@ -13,28 +13,13 @@
 #include "probe_matcher.h"
 #include "scopeguard.h"
 #include "tracefs/tracefs.h"
-#include "util/bpf_progs.h"
+#include "util/kernel.h"
 #include "util/paths.h"
 #include "util/strings.h"
 #include "util/symbols.h"
-#include "util/system.h"
 #include "util/wildcard.h"
 
-#include <bcc/bcc_elf.h>
-#include <bcc/bcc_syms.h>
-#include <elf.h>
-
 namespace bpftrace {
-
-static int add_symbol(const char* symname,
-                      uint64_t /*start*/,
-                      uint64_t /*size*/,
-                      void* payload)
-{
-  auto* syms = static_cast<std::set<std::string>*>(payload);
-  syms->insert(std::string(symname));
-  return 0;
-}
 
 // Finds all matches of search_input in the provided input stream.
 std::set<std::string> ProbeMatcher::get_matches_in_stream(
@@ -118,7 +103,7 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
       if (target.empty()) {
         symbol_stream = get_symbols_from_traceable_funcs(false);
       } else if (!util::has_wildcard(target)) {
-        symbol_stream = get_module_symbols_from_traceable_funcs(target);
+        symbol_stream = get_symbols_from_traceable_funcs(true, target);
       } else {
         symbol_stream = get_symbols_from_traceable_funcs(true);
       }
@@ -127,11 +112,44 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
     case ProbeType::uprobe:
     case ProbeType::uretprobe:
     case ProbeType::watchpoint: {
-      symbol_stream = get_func_symbols_from_file(bpftrace_->pid(), target);
+      auto result = [&]() {
+        if (bpftrace_->pid()) {
+          auto pid_symbols = user_func_info_.func_symbols_for_pid(
+              *bpftrace_->pid());
+          if (pid_symbols) {
+            return *pid_symbols;
+          }
+        }
+        return util::BinaryFuncMap();
+      }();
+      // Expand to the full set of possible matching paths.
+      auto paths = util::resolve_binary_path(target, bpftrace_->pid());
+      for (const auto& path : paths) {
+        auto ok = user_func_info_.func_symbols_for_path(path);
+        if (ok) {
+          for (const auto& sym : *ok) {
+            result[path].insert(sym);
+          }
+        }
+      }
+      std::stringstream ss;
+      for (const auto& [path, symbols] : result) {
+        for (const auto& sym : symbols) {
+          ss << path << ":" << sym << "\n";
+        }
+      }
+      symbol_stream = std::make_unique<std::istringstream>(ss.str());
       break;
     }
     case ProbeType::tracepoint: {
-      symbol_stream = get_symbols_from_file(tracefs::available_events());
+      auto result = kernel_func_info_.get_tracepoints();
+      std::stringstream ss;
+      for (const auto& [category, events] : result) {
+        for (const auto& event : *events) {
+          ss << category << ":" << event << "\n";
+        }
+      }
+      symbol_stream = std::make_unique<std::istringstream>(ss.str());
       break;
     }
     // The two `has_btf_data` checks below for fentry/fexit/rawtracepoints
@@ -154,7 +172,34 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
       break;
     }
     case ProbeType::usdt: {
-      symbol_stream = get_symbols_from_usdt(bpftrace_->pid(), target);
+      // If pid is set, then we grab symbols directly from the binary.
+      auto result = [&]() {
+        if (bpftrace_->pid()) {
+          auto pid_symbols = user_func_info_.usdt_probes_for_pid(
+              *bpftrace_->pid());
+          if (pid_symbols) {
+            return *pid_symbols;
+          }
+        }
+        return util::BinaryUSDTMap();
+      }();
+      // Like the uprobe case above, we expand to the full set of paths.
+      auto paths = util::resolve_binary_path(target, bpftrace_->pid());
+      for (const auto& path : paths) {
+        auto ok = user_func_info_.usdt_probes_for_path(path);
+        if (ok) {
+          for (const auto& sym : *ok) {
+            result[path].insert(sym);
+          }
+        }
+      }
+      std::stringstream ss;
+      for (const auto& [path, symbols] : result) {
+        for (const auto& sym : symbols) {
+          ss << path << ":" << sym.provider << ":" << sym.name + "\n";
+        }
+      }
+      symbol_stream = std::make_unique<std::istringstream>(ss.str());
       break;
     }
     case ProbeType::software: {
@@ -222,38 +267,31 @@ std::set<std::string> ProbeMatcher::get_matches_in_set(
   return get_matches_in_stream(search_input, stream, false, '$');
 }
 
-std::unique_ptr<std::istream> ProbeMatcher::get_symbols_from_file(
-    const std::string& path) const
-{
-  auto file = std::make_unique<std::ifstream>(path);
-  if (file->fail()) {
-    LOG(WARNING) << "Could not read symbols from " << path << ": "
-                 << strerror(errno);
-    return nullptr;
-  }
-
-  return file;
-}
-
 std::unique_ptr<std::istream> ProbeMatcher::get_symbols_from_traceable_funcs(
-    bool with_modules) const
+    bool with_modules,
+    std::optional<std::string> module) const
 {
-  return bpftrace_->get_traceable_funcs(with_modules);
-}
-
-std::unique_ptr<std::istream> ProbeMatcher::
-    get_module_symbols_from_traceable_funcs(const std::string& mod) const
-{
-  return bpftrace_->get_module_traceable_funcs(mod);
+  std::stringstream ss;
+  for (const auto& [mod, funcs] :
+       kernel_func_info_.get_traceable_funcs(module)) {
+    for (const auto& func : *funcs) {
+      if (with_modules) {
+        ss << mod << ":" << func << "\n";
+      } else {
+        ss << func << "\n";
+      }
+    }
+  }
+  return std::make_unique<std::istringstream>(ss.str());
 }
 
 std::unique_ptr<std::istream> ProbeMatcher::get_fentry_symbols(
     const std::string& mod) const
 {
   if (bpftrace_->btf_->has_data() && bpftrace_->btf_->modules_loaded()) {
-    return bpftrace_->btf_->get_all_funcs();
+    return bpftrace_->btf_->get_all_traceable_funcs(kernel_func_info_);
   } else if (!mod.empty() && !util::has_wildcard(mod)) {
-    return get_module_symbols_from_traceable_funcs(mod);
+    return get_symbols_from_traceable_funcs(true, mod);
   } else {
     return get_symbols_from_traceable_funcs(true);
   }
@@ -262,7 +300,7 @@ std::unique_ptr<std::istream> ProbeMatcher::get_fentry_symbols(
 std::unique_ptr<std::istream> ProbeMatcher::get_running_bpf_programs() const
 {
   std::string funcs;
-  auto ids_and_syms = util::get_bpf_progs();
+  auto ids_and_syms = kernel_func_info_.get_bpf_progs();
   for (const auto& [id, symbol] : ids_and_syms) {
     funcs += "bpf:" + std::to_string(id) + ":" + symbol + "\n";
   }
@@ -274,99 +312,15 @@ std::unique_ptr<std::istream> ProbeMatcher::get_raw_tracepoint_symbols() const
   if (bpftrace_->btf_->has_data() && bpftrace_->btf_->modules_loaded()) {
     return bpftrace_->btf_->get_all_raw_tracepoints();
   } else {
-    return bpftrace_->get_raw_tracepoints_from_traceable_funcs();
-  }
-}
-
-std::unique_ptr<std::istream> ProbeMatcher::get_func_symbols_from_file(
-    std::optional<int> pid,
-    const std::string& path) const
-{
-  if (path.empty())
-    return std::make_unique<std::istringstream>("");
-
-  auto get_paths = [&]() -> Result<std::vector<std::string>> {
-    if (path == "*") {
-      if (pid.has_value()) {
-        return util::get_mapped_paths_for_pid(*pid);
-      } else {
-        return util::get_mapped_paths_for_running_pids();
+    std::string rts;
+    for (const auto& [module, events] :
+         kernel_func_info_.get_raw_tracepoints()) {
+      for (const auto& event : *events) {
+        rts += module + ":" + event + "\n";
       }
-    } else if (path.find('*') != std::string::npos) {
-      return util::resolve_binary_path(path, pid);
-    } else {
-      return std::vector<std::string>({ path });
     }
-  };
-  auto real_paths = get_paths();
-  if (!real_paths) {
-    // This will crash, but we need to plumb the ability to return errors
-    // through these APIs. Right now we have no other choice.
-    LOG(WARNING) << "Could not resolve binary path: " << real_paths.takeError();
-    return nullptr;
+    return std::make_unique<std::istringstream>(rts);
   }
-
-  struct bcc_symbol_option symbol_option;
-  memset(&symbol_option, 0, sizeof(symbol_option));
-  symbol_option.use_debug_file = 1;
-  symbol_option.check_debug_file_crc = 1;
-  symbol_option.use_symbol_type = (1 << STT_FUNC) | (1 << STT_GNU_IFUNC);
-
-  std::string result;
-  for (auto& real_path : *real_paths) {
-    std::set<std::string> syms;
-    // Workaround: bcc_elf_foreach_sym() can return the same symbol twice if
-    // it's also found in debug info (#1138), so a std::set is used here
-    // (and in the add_symbol callback) to ensure that each symbol will be
-    // unique in the returned string.
-    int err = bcc_elf_foreach_sym(
-        real_path.c_str(), add_symbol, &symbol_option, &syms);
-    if (err) {
-      LOG(WARNING) << "Could not list function symbols: " + real_path;
-    }
-    for (const auto& sym : syms)
-      result += real_path + ":" + sym + "\n";
-  }
-  return std::make_unique<std::istringstream>(result);
-}
-
-std::unique_ptr<std::istream> ProbeMatcher::get_symbols_from_usdt(
-    std::optional<int> pid,
-    const std::string& target) const
-{
-  std::string probes;
-  util::usdt_probe_list usdt_probes;
-
-  if (pid.has_value())
-    usdt_probes = USDTHelper::probes_for_pid(
-        *pid, bpftrace_->feature_->has_uprobe_multi());
-  else if (target == "*")
-    usdt_probes = USDTHelper::probes_for_all_pids(
-        bpftrace_->feature_->has_uprobe_multi());
-  else if (!target.empty()) {
-    std::vector<std::string> real_paths;
-    if (target.find('*') != std::string::npos)
-      real_paths = util::resolve_binary_path(target);
-    else
-      real_paths.push_back(target);
-
-    for (auto& real_path : real_paths) {
-      auto target_usdt_probes = USDTHelper::probes_for_path(
-          real_path, bpftrace_->feature_->has_uprobe_multi());
-      usdt_probes.insert(usdt_probes.end(),
-                         target_usdt_probes.begin(),
-                         target_usdt_probes.end());
-    }
-  }
-
-  for (auto const& usdt_probe : usdt_probes) {
-    std::string path = usdt_probe.path;
-    std::string provider = usdt_probe.provider;
-    std::string fname = usdt_probe.name;
-    probes += path + ":" + provider + ":" + fname + "\n";
-  }
-
-  return std::make_unique<std::istringstream>(probes);
 }
 
 std::unique_ptr<std::istream> ProbeMatcher::get_symbols_from_list(
@@ -601,7 +555,7 @@ std::set<std::string> ProbeMatcher::get_matches_for_ap(
     }
     case ProbeType::usdt: {
       auto target = attach_point.target;
-      // If PID is specified, targets in symbol_stream will have the
+      // If PID is specified, targets in symbol_stream may have the
       // "/proc/<PID>/root" prefix followed by an absolute path, so we make
       // the target absolute and add a leading wildcard.
       if (bpftrace_->pid().has_value()) {

src/CMakeLists.txt
@@ -37,7 +37,6 @@ add_library(runtime STATIC
   probe_types.cpp
   procmon.cpp
   run_bpftrace.cpp
-  usdt.cpp
   pcap_writer.cpp
   types_format.cpp
   ksyms.cpp

src/ast/ast.h
@@ -15,7 +15,7 @@
 #include "diagnostic.h"
 #include "probe_types.h"
 #include "types.h"
-#include "usdt.h"
+#include "util/elf_parser.h"
 #include "util/strings.h"
 
 namespace bpftrace::ast {

src/ast/passes/ap_probe_expansion.cpp
@@ -2,8 +2,10 @@
 
 #include <algorithm>
 
+#include "ast/passes/attachpoint_passes.h"
 #include "ast/visitor.h"
 #include "bpftrace.h"
+#include "probe_matcher.h"
 #include "util/wildcard.h"
 
 namespace bpftrace::ast {
@@ -212,8 +214,15 @@ class ProbeAndApExpander : public Visitor<ProbeAndApExpander> {
 public:
   ProbeAndApExpander(ASTContext &ast,
                      BPFtrace &bpftrace,
+                     FunctionInfo &func_info,
                      ExpansionResult &result)
-      : ast_(ast), bpftrace_(bpftrace), result_(result)
+      : ast_(ast),
+        bpftrace_(bpftrace),
+        func_info_(func_info),
+        result_(result),
+        probe_matcher_(&bpftrace,
+                       func_info.kernel_function_info(),
+                       func_info.user_function_info())
   {
   }
 
@@ -228,7 +237,9 @@ private:
 
   ASTContext &ast_;
   BPFtrace &bpftrace_;
+  FunctionInfo &func_info_;
   ExpansionResult &result_;
+  ProbeMatcher probe_matcher_;
 };
 
 void ProbeAndApExpander::expand()
@@ -270,7 +281,7 @@ void ProbeAndApExpander::visit(AttachPointList &aps)
     auto expansion = result_.get_expansion(*ap);
     switch (expansion) {
       case ExpansionType::FULL: {
-        auto matches = bpftrace_.probe_matcher_->get_matches_for_ap(*ap);
+        auto matches = probe_matcher_.get_matches_for_ap(*ap);
 
         probe_count_ += matches.size();
         if (probe_count_ > max_bpf_progs) {
@@ -293,7 +304,7 @@ void ProbeAndApExpander::visit(AttachPointList &aps)
 
       case ExpansionType::SESSION:
       case ExpansionType::MULTI: {
-        auto matches = bpftrace_.probe_matcher_->get_matches_for_ap(*ap);
+        auto matches = probe_matcher_.get_matches_for_ap(*ap);
         if (util::has_wildcard(ap->target)) {
           // If we have a wildcard in the target path, we need to generate one
           // attach point per expanded target
@@ -330,7 +341,7 @@ void ProbeAndApExpander::visit(AttachPointList &aps)
           const std::string &funcname = ap->func;
           const std::string &modname = ap->target;
           if ((!modname.empty()) && modname != "vmlinux") {
-            if (!bpftrace_.is_module_loaded(modname)) {
+            if (!func_info_.kernel_function_info().is_module_loaded(modname)) {
               ap->addError() << "specified module " + modname + " in probe " +
                                     ap->provider + ":" + modname + ":" +
                                     funcname + " is not loaded.";
@@ -338,7 +349,7 @@ void ProbeAndApExpander::visit(AttachPointList &aps)
           }
         }
 
-        auto matches = bpftrace_.probe_matcher_->get_matches_for_ap(*ap);
+        auto matches = probe_matcher_.get_matches_for_ap(*ap);
         // Filter out unnecessary probes, as they may not be missing.
         if (matches.empty() && (pt != ProbeType::watchpoint)) {
           const auto missing_probes = bpftrace_.config_->missing_probes;
@@ -364,14 +375,14 @@ void ProbeAndApExpander::visit(AttachPointList &aps)
 
 Pass CreateProbeAndApExpansionPass()
 {
-  auto fn = [](ASTContext &ast, BPFtrace &bpftrace) {
+  auto fn = [](ASTContext &ast, BPFtrace &bpftrace, FunctionInfo &func_info) {
     ExpansionAnalyser analyser(bpftrace);
     auto result = analyser.analyse(*ast.root);
 
     SessionExpander session_expander(ast, bpftrace, result);
     session_expander.visit(*ast.root);
 
-    ProbeAndApExpander expander(ast, bpftrace, result);
+    ProbeAndApExpander expander(ast, bpftrace, func_info, result);
     expander.expand();
 
     return result;

src/ast/passes/args_resolver.cpp
@@ -3,6 +3,7 @@
 
 #include "arch/arch.h"
 #include "ast/passes/args_resolver.h"
+#include "ast/passes/attachpoint_passes.h"
 #include "ast/passes/tracepoint_format_parser.h"
 #include "ast/visitor.h"
 #include "bpftrace.h"
@@ -29,7 +30,8 @@ namespace {
 
 class ArgsResolver : public Visitor<ArgsResolver> {
 public:
-  explicit ArgsResolver(BPFtrace &bpftrace) : bpftrace_(bpftrace) {};
+  explicit ArgsResolver(BPFtrace &bpftrace, FunctionInfo &func_info)
+      : bpftrace_(bpftrace), func_info_(func_info) {};
 
   using Visitor<ArgsResolver>::visit;
   void visit(Builtin &builtin);
@@ -40,6 +42,7 @@ private:
   Result<std::shared_ptr<Struct>> resolve_args(const AttachPoint &ap);
 
   BPFtrace &bpftrace_;
+  FunctionInfo &func_info_;
   Probe *probe_ = nullptr;
 };
 
@@ -59,10 +62,14 @@ Result<std::shared_ptr<Struct>> ArgsResolver::resolve_args(
   switch (probe_type) {
     case ProbeType::fentry:
     case ProbeType::fexit:
-      return bpftrace_.btf_->resolve_args(
-          ap.func, probe_type == ProbeType::fexit, true, false);
+      return bpftrace_.btf_->resolve_args(ap.func,
+                                          probe_type == ProbeType::fexit,
+                                          true,
+                                          false,
+                                          func_info_.kernel_function_info());
     case ProbeType::rawtracepoint:
-      return bpftrace_.btf_->resolve_raw_tracepoint_args(ap.func);
+      return bpftrace_.btf_->resolve_raw_tracepoint_args(
+          ap.func, func_info_.kernel_function_info());
     case ProbeType::tracepoint: {
       TracepointFormatParser parser(ap.target, ap.func, bpftrace_);
       auto ok = parser.parse_format_file();
@@ -123,8 +130,8 @@ void ArgsResolver::visit(Probe &probe)
 
 Pass CreateArgsResolverPass()
 {
-  auto fn = [](ASTContext &ast, BPFtrace &b) {
-    ArgsResolver resolver(b);
+  auto fn = [](ASTContext &ast, BPFtrace &b, FunctionInfo &func_info) {
+    ArgsResolver resolver(b, func_info);
     resolver.visit(ast.root);
   };
 

src/ast/passes/attachpoint_passes.cpp
@@ -11,6 +11,7 @@
 #include "ast/helpers.h"
 #include "ast/passes/attachpoint_passes.h"
 #include "ast/visitor.h"
+#include "probe_matcher.h"
 #include "util/int_parser.h"
 #include "util/paths.h"
 #include "util/strings.h"
@@ -22,17 +23,19 @@ namespace bpftrace::ast {
 class AttachPointChecker : public Visitor<AttachPointChecker> {
 public:
   explicit AttachPointChecker(BPFtrace &bpftrace,
-                              bool listing,
-                              bool has_child = true)
-      : bpftrace_(bpftrace), listing_(listing), has_child_(has_child) {};
+                              FunctionInfo &func_info_state,
+                              bool listing)
+      : bpftrace_(bpftrace),
+        func_info_state_(func_info_state),
+        listing_(listing) {};
 
   using Visitor<AttachPointChecker>::visit;
   void visit(AttachPoint &ap);
 
 private:
   BPFtrace &bpftrace_;
+  FunctionInfo &func_info_state_;
   bool listing_;
-  bool has_child_ = false;
   std::unordered_map<std::string, Location> test_locs_;
   std::unordered_map<std::string, Location> benchmark_locs_;
 };
@@ -44,7 +47,8 @@ void AttachPointChecker::visit(AttachPoint &ap)
       ap.addError() << "kprobes should be attached to a function";
     // Warn if user tries to attach to a non-traceable function
     if (bpftrace_.config_->missing_probes != ConfigMissingProbes::ignore &&
-        !util::has_wildcard(ap.func) && !bpftrace_.is_traceable_func(ap.func)) {
+        !util::has_wildcard(ap.func) &&
+        !func_info_state_.kernel_function_info().is_traceable(ap.func)) {
       ap.addWarning() << ap.func
                       << " is not traceable (either non-existing, inlined, "
                          "or marked as "
@@ -133,13 +137,24 @@ void AttachPointChecker::visit(AttachPoint &ap)
 
     const auto pid = bpftrace_.pid();
     if (pid.has_value()) {
-      USDTHelper::probes_for_pid(*pid, bpftrace_.feature_->has_uprobe_multi());
+      auto ok = func_info_state_.user_function_info().usdt_probes_for_pid(*pid);
+      if (!ok && !listing_) {
+        ap.addError() << ok.takeError();
+      }
     } else if (ap.target == "*") {
-      USDTHelper::probes_for_all_pids(bpftrace_.feature_->has_uprobe_multi());
+      auto ok =
+          func_info_state_.user_function_info().usdt_probes_for_all_pids();
+      if (!ok && !listing_) {
+        ap.addError() << ok.takeError();
+      }
     } else if (!ap.target.empty()) {
-      for (auto &path : util::resolve_binary_path(ap.target))
-        USDTHelper::probes_for_path(path,
-                                    bpftrace_.feature_->has_uprobe_multi());
+      for (auto &path : util::resolve_binary_path(ap.target)) {
+        auto ok = func_info_state_.user_function_info().usdt_probes_for_path(
+            path);
+        if (!ok && !listing_) {
+          ap.addError() << ok.takeError();
+        }
+      }
     } else {
       ap.addError() << "usdt probe must specify at least path or pid to "
                        "probe. To target "
@@ -329,8 +344,12 @@ AttachPointParser::State AttachPointParser::argument_count_error(
 
 AttachPointParser::AttachPointParser(ASTContext &ctx,
                                      BPFtrace &bpftrace,
+                                     FunctionInfo &func_info_state,
                                      bool listing)
-    : ctx_(ctx), bpftrace_(bpftrace), listing_(listing)
+    : ctx_(ctx),
+      bpftrace_(bpftrace),
+      func_info_state_(func_info_state),
+      listing_(listing)
 {
 }
 
@@ -419,13 +438,14 @@ AttachPointParser::State AttachPointParser::parse_attachpoint(AttachPoint &ap)
     // If PID is specified or the second part of the attach point is a path
     // (contains '/'), use userspace probe types.
     // Otherwise, use kernel probe types.
+    ProbeMatcher probe_matcher(&bpftrace_,
+                               func_info_state_.kernel_function_info(),
+                               func_info_state_.user_function_info());
     if (bpftrace_.pid().has_value() ||
         (parts_.size() >= 2 && parts_[1].find('/') != std::string::npos)) {
-      probe_types = bpftrace_.probe_matcher_->expand_probetype_userspace(
-          probetype_query);
+      probe_types = probe_matcher.expand_probetype_userspace(probetype_query);
     } else {
-      probe_types = bpftrace_.probe_matcher_->expand_probetype_kernel(
-          probetype_query);
+      probe_types = probe_matcher.expand_probetype_kernel(probetype_query);
     }
   } else
     probe_types = { front };
@@ -996,7 +1016,8 @@ AttachPointParser::State AttachPointParser::fentry_parser()
   } else {
     ap_->func = parts_[1];
     if (!util::has_wildcard(ap_->func)) {
-      auto func_modules = bpftrace_.get_func_modules(ap_->func);
+      auto func_modules =
+          func_info_state_.kernel_function_info().get_func_modules(ap_->func);
       if (func_modules.size() == 1)
         ap_->target = *func_modules.begin();
       else if (func_modules.size() > 1) {
@@ -1077,21 +1098,22 @@ AttachPointParser::State AttachPointParser::raw_tracepoint_parser()
 // Note: listing changes the parsing semantics for attach points
 Pass CreateParseAttachpointsPass(bool listing)
 {
-  return Pass::create("parse-attachpoints",
-                      [listing](ASTContext &ast, BPFtrace &b) {
-                        AttachPointParser ap_parser(ast, b, listing);
-                        ap_parser.parse();
-                      });
+  return Pass::create(
+      "parse-attachpoints",
+      [listing](ASTContext &ast, BPFtrace &b, FunctionInfo &func_info) {
+        AttachPointParser ap_parser(ast, b, func_info, listing);
+        ap_parser.parse();
+      });
 }
 
 Pass CreateCheckAttachpointsPass(bool listing)
 {
-  return Pass::create("check-attachpoints",
-                      [listing](ASTContext &ast, BPFtrace &b) {
-                        AttachPointChecker ap_checker(
-                            b, listing, !b.cmd_.empty() || b.child_ != nullptr);
-                        ap_checker.visit(*ast.root);
-                      });
+  return Pass::create(
+      "check-attachpoints",
+      [listing](ASTContext &ast, BPFtrace &b, FunctionInfo &func_info) {
+        AttachPointChecker ap_checker(b, func_info, listing);
+        ap_checker.visit(*ast.root);
+      });
 }
 
 } // namespace bpftrace::ast

src/ast/passes/attachpoint_passes.h
@@ -7,12 +7,34 @@
 #include "ast/ast.h"
 #include "ast/pass_manager.h"
 #include "bpftrace.h"
+#include "util/kernel.h"
+#include "util/user.h"
 
 namespace bpftrace::ast {
 
+// Provides function information (kernel and user space) to AST passes.
+class FunctionInfo : public bpftrace::ast::State<"function-info"> {
+public:
+  explicit FunctionInfo(util::KernelFunctionInfo &kernel_impl,
+                        util::UserFunctionInfo &user_impl)
+      : kernel_impl_(kernel_impl), user_impl_(user_impl)
+  {
+  }
+
+  util::KernelFunctionInfo &kernel_function_info() const { return kernel_impl_; }
+  util::UserFunctionInfo &user_function_info() const { return user_impl_; }
+
+private:
+  util::KernelFunctionInfo &kernel_impl_;
+  util::UserFunctionInfo &user_impl_;
+};
+
 class AttachPointParser {
 public:
-  AttachPointParser(ASTContext &ctx, BPFtrace &bpftrace, bool listing);
+  AttachPointParser(ASTContext &ctx,
+                    BPFtrace &bpftrace,
+                    FunctionInfo &func_info_state,
+                    bool listing);
   ~AttachPointParser() = default;
   void parse();
 
@@ -59,6 +81,7 @@ private:
 
   ASTContext &ctx_;
   BPFtrace &bpftrace_;
+  FunctionInfo &func_info_state_;
   AttachPoint *ap_{ nullptr }; // Non-owning pointer
   std::stringstream errs_;
   std::vector<std::string> parts_;

src/ast/passes/codegen_llvm.h
@@ -9,7 +9,6 @@
 
 #include "ast/pass_manager.h"
 #include "ast/passes/link.h"
-#include "usdt.h"
 
 namespace bpftrace::ast {
 

commit 3d59d59cae1e0ae728b3e5f9e4ca82f873ebc8b2

probe_matcher: fix probe listing for -p
bpftrace -l -p <pid> with target "*" is supposed to list only probes
from the running pid.
With 4a3b4dec this changed, so that currently all binaries from $PATH
are enumerated. Add back the restriction.

Fixes: 4a3b4dec ("cleanup: aggregate function info into mockable interfaces")
Signed-off-by: Arne Jansen <arne@die-jansens.de>

src/probe_matcher.cpp
@@ -123,15 +123,19 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
         }
         return symbols::BinaryFuncMap();
       }();
-      // Expand to the full set of possible matching paths.
-      auto paths = util::resolve_binary_path(target,
-                                             bpftrace_->pid(),
-                                             bpftrace_->safe_mode_);
-      for (const auto& path : paths) {
-        auto ok = user_func_info_.func_symbols_for_path(path);
-        if (ok) {
-          for (const auto& sym : *ok) {
-            result[path].insert(sym);
+      // Expand to the full set of possible matching paths, unless a pid is
+      // given with -p and target is '*'. In that case, we already have
+      // the full set of symbols.
+      if (!bpftrace_->pid() || target != "*") {
+        auto paths = util::resolve_binary_path(target,
+                                               bpftrace_->pid(),
+                                               bpftrace_->safe_mode_);
+        for (const auto& path : paths) {
+          auto ok = user_func_info_.func_symbols_for_path(path);
+          if (ok) {
+            for (const auto& sym : *ok) {
+              result[path].insert(sym);
+            }
           }
         }
       }